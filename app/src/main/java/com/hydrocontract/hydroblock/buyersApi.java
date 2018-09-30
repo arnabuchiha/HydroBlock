6fbb8b65e84dd4d7adb102992f93af0768c509e1
ShadowRajnsih <rr66520@gmail.com>
2018-09-30 07:40:23 +0530

    buyerApi

---
+++ app/src/main/java/com/hydrocontract/hydroblock/buyersApi.java
@@ -0,0 +1,12 @@
+package com.hydrocontract.hydroblock;
+
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
+public interface buyersApi {
+
+    String Base_Url="http://13.126.150.180:3000/";
+    @FormUrlEncoded
+    @POST("")
+}
