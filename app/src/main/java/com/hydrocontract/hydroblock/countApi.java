0f811db91dc2b73d06ce9a651370ac8528318b41
Arnab Ray <arnabkeya04@gmail.com>
2018-10-01 16:57:16 +0530

    Update

--- app/src/main/java/com/hydrocontract/hydroblock/countApi.java
+++ app/src/main/java/com/hydrocontract/hydroblock/countApi.java
@@ -5,7 +5,7 @@ import retrofit2.http.POST;
 
 public interface countApi {
 
-    String Base_url="http://13.126.150.180:3000/";
+    String Base_url="Url";
     @POST("seller/count")
     Call<countPojo> getCount();
 }
