868f42d8c015a6c7405d9adc49d82d37206c8b1f
Arnab Ray <arnabkeya04@gmail.com>
2018-10-01 16:56:24 +0530

    Update

--- app/src/main/java/com/hydrocontract/hydroblock/buyApi.java
+++ app/src/main/java/com/hydrocontract/hydroblock/buyApi.java
@@ -6,7 +6,7 @@ import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.POST;
 
 public interface buyApi {
-    String BASE_URL="http://13.126.150.180:3000/";
+    String BASE_URL="Type ur url";
     @FormUrlEncoded
     @POST("buyFrom")
     Call<SuccessResponse> getResponse(@Field("sellerAddress")String sellerAddress,
